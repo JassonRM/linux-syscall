f494add6661d1056cf46cd31a803b41cece8b593
Jasson Rodriguez <jassonrm@icloud.com>
2020-10-03 22:57:10 -0600

    Read strace output

--- app/main.c
+++ app/main.c
@@ -1,6 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main() {
-    printf("Hello, World!\n");
-    return 0;
+int main( int argc, char *argv[] )
+{
+    if(argc != 3){
+        return 1;
+    }
+    char path[100];
+    snprintf(path, sizeof path, "%s%s%s", "strace ", argv[2], " 2>&1");
+    printf("Executing %s\n", path);
+
+    FILE *fp;
+    char line[1035];
+
+    /* Open the command for reading. */
+    fp = popen(path, "r");
+    if (fp == NULL) {
+        printf("Failed to run command\n" );
+        exit(1);
+    }
+
+    /* Read the output a line at a time - output it. */
+    while (fgets(line, sizeof(line), fp) != NULL) {
+        printf("%s", line);
+//        Process each line here
+    }
+
+    /* close */
+    pclose(fp);
 }
--- copy_all.sh
+++ copy_all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Copying files to " $1
-cp -rfv custom-trace $1
+cp -rfv custom-trace Makefile $1
 cd arch-x86-entry-syscalls
 cp -fv * $1/arch/x86/entry/syscalls
 cd ..
